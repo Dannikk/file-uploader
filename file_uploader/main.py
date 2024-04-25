bbee8dd3c540a6bcd85c56ea26ffe0ed31e722b4
Liya <55926224+liyayunusova@users.noreply.github.com>
2024-04-26 01:13:13 +0300

    develop

--- file_uploader/main.py
+++ file_uploader/main.py
@@ -4,13 +4,21 @@ import yaml
 
 
 def get_client():
+    cfg = load_config()
     session = boto3.session.Session()
     s3 = session.client(
         service_name='s3',
         endpoint_url='https://storage.yandexcloud.net',
-        aws_access_key_id=os.getenv("aws_access_key_id"),
-        aws_secret_access_key=os.getenv("aws_secret_access_key"),
+        aws_access_key_id=cfg["aws_access_key_id"],
+        aws_secret_access_key=cfg["aws_secret_access_key"],
     )
+
+    # ooo = s3.get_paginator('list_objects').paginate(Bucket='bucket-0')
+    # print(ooo)
+    # for item in ooo:
+    #     print(item)
+    # for key in s3.list_objects(Bucket='bucket-0')['Contents']:
+    #     print(key, key['Key'])
 
     return s3
 
@@ -19,13 +27,21 @@ def upload(client, path: str):
     print(client.upload_file(path, 'bucket-0', path.split('/')[-1]))
 
 
+def download(client):
+    print(client.download_file('bucket-0', 'test_file_other.txt', 'test_file_other_downloaded.txt'))
+
+
 def paginator(client):
+    files = []
     for key in client.list_objects(Bucket='bucket-0')['Contents']:
-        print(key['Key'])
+        file_name = key['Key']
+        print(file_name)
+        files.append(file_name)
+    return files
 
 
 def load_config():
-    with open("../config/config_example.yaml", 'r') as file:
+    with open("./config/config.yaml", 'r') as file:
         config = yaml.safe_load(file)
     return config
 
@@ -34,5 +50,7 @@ if __name__ == '__main__':
     print(os.listdir("../"))
     client = get_client()
 
-    upload(client, "../test_file.txt")
-    # paginator(client)
+
+    # upload(client, "./test_file_other.txt")
+    paginator(client)
+    # download(client)
--- file_uploader/uploader.py
+++ file_uploader/uploader.py
@@ -1,21 +1,33 @@
 import streamlit as st
 import pandas as pd
 from io import StringIO
+from main import get_client, paginator
+from boto3 import s3
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+upload_tab, download_tab = st.tabs(["Upload", "Download"])
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+client = None
+if 'client' not in st.session_state:
+    client = get_client()
+    print(client)
+    st.session_state['client'] = client
+    print("Successfully created client")
+else:
+    client = st.session_state['client']
+    print("Using existing client")
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
 
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+with upload_tab:
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        client.upload_fileobj(uploaded_file, 'bucket-0', uploaded_file.name)
+        print(uploaded_file, dir(uploaded_file))
+        st.write(uploaded_file)
+        st.write(uploaded_file.name)
+
+with download_tab:
+    files = paginator(client)
+    st.write(files)
+    st.write("Download file")
+    st.write_stream(files)
+    st.table(files)
